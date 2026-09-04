30f25d45cd4066dc672982b16a5b6b22de34bed7
Gianni Carlo <gcarlo89@hotmail.com>
2026-09-04 11:13:35 -0500

    release: 1.2.0 (app 21, wear 100011)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.tortugapower.audiobookplayer"
         minSdk = 28
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.1.3"
+        versionCode = 21
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
